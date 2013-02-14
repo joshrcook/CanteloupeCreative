0b6f51dd52d271fa2772dbbefb61ea5e98f780a4
Mingei Website <jyork@mingei.org>
2013-02-14 00:48:13 -0500

    Changes to script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 jQuery(document).ready(function($) {
 	// initialize stellar
-        $(window).stellar({
+
+        $(window).load(function() {
+            console.log('loaded.');
+            $(window).stellar({
             verticalOffset: -1500
+            });
         });
+        
         
         // set the height of the front page to the height of the screen
         var front_page = $('#front-page');
@@ -10,24 +15,10 @@ jQuery(document).ready(function($) {
         resize_to_window_height(last_page);
         resize_to_window_height(front_page);
         function resize_to_window_height(selector) {
-            console.log(selector);
             selector.css('height', ($(window).height()));
             $(window).resize(function() {
                 selector.css('height', ($(window).height()));
             });
         }
-        
-        
-        
-        $(window).scroll(function() {
-            
-        });
-        /*
-        var thisHeight;
-        $('.picture-text').each(function() {
-            thisHeight = ($(this).height())/2*(-1);
-            $('.picture-text').css('marginTop', thisHeight);
-        });
-        */
         
 });
